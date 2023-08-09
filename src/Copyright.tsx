1d12e8f09763c7b7cf064dbbc12e9323a9e2c21c
dang_ya_ming <dangyaming@outlook.com>
2023-08-09 15:20:23 +0800

    update

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -6,8 +6,8 @@ export default function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <MuiLink color="inherit" href="https://mui.com/">
-        Your Website
+      <MuiLink color="inherit" href="https://www.aisheishei.me/">
+        爱谁谁
       </MuiLink>{' '}
       {new Date().getFullYear()}.
     </Typography>
