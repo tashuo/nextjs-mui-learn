3755b32e127e6a0f3eb819340eea767b6e79d3df
dang_ya_ming <dangyaming@outlook.com>
2025-01-16 00:16:45 -0500

    feat: updae copyright

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -6,8 +6,8 @@ export default function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <MuiLink color="inherit" href="https://www.aisheishei.me/">
-        爱谁谁
+      <MuiLink color="inherit" href="#">
+        Qilin
       </MuiLink>{' '}
       {new Date().getFullYear()}.
     </Typography>
